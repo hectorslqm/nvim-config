4127b6048a51a05f777572efdbbc66386dcc13ed
Hector S Lazcano Quintero M <hectorslqm@gmail.com>
2026-09-05 16:26:09 -0500

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- cursor
+vim.opt.guicursor = "n-v-c-i-ci-ve:ver25,r-cr:hor20,o:hor50"
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "Ferouk/bearded-nvim",
+    name = "bearded",
+    priority = 1000,
+    opts = {
+      flavor = "feat-mellejulie-light",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "bearded",
+    },
+  },
+}
